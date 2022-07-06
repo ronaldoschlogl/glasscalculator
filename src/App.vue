f39e858597efb2a906037e592ec6705b932db08f
Ronaldo <ronaldogmschlogl@gmail.com>
2022-07-06 00:02:10 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,32 @@
         <div class="glass">
             <div class="calculator">
                 <div class="calc">
-                    {{ calc }}
+                    {{ calculatorValue }}
                 </div>
                 <div class="value">
-                    {{ value }}
+                    {{ previousCalculatorValue }}
                 </div>
                 <div class="buttons">
                     <div
-                        v-for="x in 10"
-                        v-on:click="setModifier(x - 1)"
-                        class="btn"
-                        :key="x"
+                        class="button"
+                        v-for="n in btnArr"
+                        :key="n"
+                        :class="{
+                            operator: [
+                                'C',
+                                '*',
+                                '/',
+                                '-',
+                                '+',
+                                '%',
+                                '=',
+                            ].includes(n),
+                        }"
                     >
-                        {{ x - 1 }}
+                        <div class="btn" @click="action(n)">
+                            {{ n }}
+                        </div>
                     </div>
-                    <div v-on:click="add()" class="btn">+</div>
-                    <div v-on:click="sub()" class="btn">-</div>
-                    <div v-on:click="divide()" class="btn">/</div>
-                    <div v-on:click="mult()" class="btn">x</div>
-                    <div v-on:click="calculate()" class="btn">=</div>
-                    <div v-on:click="reset()" class="btn">C</div>
                 </div>
             </div>
         </div>
@@ -34,33 +40,56 @@ export default {
     name: "App",
     data: function () {
         return {
-            calc: "",
-            value: null,
+            calculatorValue: "",
+            btnArr: [
+                "C",
+                "*",
+                "/",
+                "-",
+                "7",
+                "8",
+                "9",
+                "+",
+                "4",
+                "5",
+                "6",
+                "%",
+                "1",
+                "2",
+                "3",
+                "=",
+                "0",
+                ".",
+            ],
+            operator: null,
+            previousCalculatorValue: "",
         };
     },
     methods: {
-        add() {
-            this.calc += "+";
-        },
-        sub() {
-            this.calc += "-";
-        },
-        mult() {
-            this.calc += "*";
-        },
-        divide() {
-            this.calc += "/";
-        },
-        setModifier(x) {
-            this.calc += x;
-        },
-        calculate() {
-            this.value = eval(this.calc);
-            this.calc = this.value;
-        },
-        reset() {
-            this.value = null;
-            this.calc = "";
+        action(n) {
+            if (!isNaN(n) || n === ".") {
+                this.calculatorValue += n + "";
+            }
+            if (n === "C") {
+                this.calculatorValue = "";
+            }
+            if (n === "%") {
+                this.calculatorValue = this.calculatorValue / 100 + "";
+            }
+            if (["/", "*", "-", "+"].includes(n)) {
+                this.operator = n;
+                this.previousCalculatorValue = this.calculatorValue;
+                this.calculatorValue = "";
+            }
+            if (n === "=") {
+                this.calculatorValue = eval(
+                    this.previousCalculatorValue +
+                        this.operator +
+                        this.calculatorValue
+                );
+                this.previousCalculatorValue = "";
+                this.operator = null;
+            }
         },
     },
 };
@@ -80,7 +109,7 @@ body {
     background-image: url("./assets/bg.jpg");
     background-size: cover;
     background-repeat: no-repeat;
-    color: rgba(255, 255, 255, 1);
+    color: #fff;
     font-family: "Rajdhani", sans-serif;
     position: fixed;
     top: 0;
@@ -93,12 +122,12 @@ body {
 }
 
 .calculator {
-    width: 320px;
-    background: rgba(255, 255, 255, 0.1);
+    width: 100%;
+    background: rgba(32, 32, 32, 0.1);
     border-radius: 10px;
     justify-content: center;
     align-items: center;
-    backdrop-filter: blur(5px);
+    backdrop-filter: blur(10px);
     box-shadow: 0 25px 45px rgba(0, 0, 0, 0.1);
     border: 1px solid rgba(255, 255, 255, 0.5);
     border-right: 1px solid rgba(255, 255, 255, 0.2);
@@ -106,20 +135,26 @@ body {
 }
 
 .buttons {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px, auto);
     width: 280px;
+    border-radius: 4px;
+    align-items: center;
+    justify-content: center;
+    margin: 10px auto;
+    gap: 5px;
 }
 
 .btn {
-    flex: 0 0 93px;
-    height: 63px;
+    padding: 6px;
     border: 1px solid transparent;
-    line-height: 61px;
-    transition: all 0.5s;
     border-radius: 3px;
-
+    margin: 4px;
+    font-size: 1.5em;
+    text-align: center;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
     &:hover {
         background-color: rgba(255, 255, 255, 0.2);
         border-color: rgba(255, 255, 255, 0.1);
@@ -129,76 +164,9 @@ body {
     }
 }
 
-.value{
-  height: 50px;
-  line-height: 50px;
-  font-size: 30px;
+.value {
+    height: 50px;
+    line-height: 50px;
+    font-size: 30px;
 }
-// body {
-//     margin: 0;
-//     padding: 0;
-// }
-// .container {
-//     width: 100vw;
-//     height: 100vh;
-//     background-image: url("./assets/bg.jpg");
-//     background-position: center;
-//     background-size: cover;
-//     background-repeat: no-repeat;
-//     display: flex;
-//     flex-direction: column;
-//     align-content: center;
-//     justify-content: center;
-//     align-items: center;
-//     @media only screen and (min-width: 480px) {
-//         flex-direction: row;
-//     }
-//     .glass {
-//         width: 100%;
-//         max-width: 350px;
-//         height: 250px;
-//         margin: 20px;
-//         border: 2px solid rgba(255, 255, 255, 0.2);
-//         background: rgba(255, 255, 255, 0.4);
-//         border-radius: 10px;
-//         z-index: 2;
-//         overflow: hidden;
-//         backdrop-filter: blur(5px);
-//         -webkit-backdrop-filter: blur(5px);
-//     }
-//     .glass {
-//         border: 2px solid rgba(0, 0, 0, 0.2);
-//         background: rgba(0, 0, 0, 0.4);
-//     }
-
-//     .calculator{
-//       justify-content: center;
-//       align-items: center;
-
-//       .buttons{
-//         display: flex;
-//         flex-wrap: wrap;
-//         justify-content: center;
-//         width: 280px;
-
-//         .btn{
-//           flex: 0 0 93px;
-//           height: 63px;
-//           border: 1px solid transparent;
-//           line-height: 61px;
-//           transition: all 0.5s;
-//           border-radius: 3px;
-
-//           &:hover{
-//             background-color: rgba(255, 255, 255, 0.2);
-//             border-color: rgba(255, 255, 255, 0.1);
-//             color: #fff;
-//             cursor: pointer;
-//             transition: all 0.3s;
-//           }
-//         }
-//       }
-
-//     }
-// }
 </style>
